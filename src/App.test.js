c418f80461148c2cb184157790f6bce63a90723b
taohui <htao@thoughtworks.com>
2022-10-08 15:46:44 +0800

    integrate with travis ci and heroku

---
+++ src/App.test.js
@@ -0,0 +1,10 @@
+import {render} from "@testing-library/react";
+import App from "./App";
+
+
+describe("App component", ()=>{
+    it("should render app without error", ()=>{
+        const { asFragment }  = render(<App />);
+        expect(asFragment()).toMatchSnapshot()
+    })
+})
